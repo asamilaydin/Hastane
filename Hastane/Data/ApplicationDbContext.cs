ed65d53ad1012bfb07e9e1e83bd73be7a17734b0
asamilaydin <101115837+asamilaydin@users.noreply.github.com>
2023-12-21 17:04:04 +0300

    Add files via upload

---
+++ Hastane/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Hastane.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hastane.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<User> Users { get; set; }
+
+}
+

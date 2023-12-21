c1115d03e428f78f63355ffbb6a836de11adb607
asamilaydin <101115837+asamilaydin@users.noreply.github.com>
2023-12-21 17:05:02 +0300

    Add files via upload

---
+++ Hastane/Controllers/HesapController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Hastane.Data;
+using Hastane.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Hastane.Controllers
+{
+    public class HesapController : Controller
+    {
+
+        private readonly ApplicationDbContext _databaseContext;
+
+        public HesapController(ApplicationDbContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+            
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+            
+        }
+
+
+    }
+}
+
+

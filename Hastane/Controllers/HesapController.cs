dbb5f98d75cb0b64a1a95a48704558f33d6dc9fc
asamilaydin <101115837+asamilaydin@users.noreply.github.com>
2023-12-22 21:33:41 +0300

    Add files via upload

--- Hastane/Controllers/HesapController.cs
+++ Hastane/Controllers/HesapController.cs
@@ -2,24 +2,30 @@
 using Hastane.Data;
 using Hastane.Models;
 using Microsoft.AspNetCore.Mvc;
+using NETCore.Encrypt.Extensions;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using System.ComponentModel.DataAnnotations;
+using Hastane.Entities;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
 
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Hastane.Controllers
 {
+    
     public class HesapController : Controller
     {
-
         private readonly ApplicationDbContext _databaseContext;
+        private readonly IConfiguration _configuration;
 
-        public HesapController(ApplicationDbContext databaseContext)
+        public HesapController(ApplicationDbContext databaseContext, IConfiguration configuration)
         {
             _databaseContext = databaseContext;
-            
+            _configuration = configuration;
         }
-
-
 
         public IActionResult Index()
         {
@@ -36,7 +42,36 @@ namespace Hastane.Controllers
         {
             if (ModelState.IsValid)
             {
+                string hashedPassword = DoMD5HashedString(model.Password);
 
+                User user = _databaseContext.Users.SingleOrDefault(x => x.Username.ToLower() == model.Username.ToLower() && x.Password == hashedPassword);
+
+                if (user != null)
+                {
+                    if (user.Locked)
+                    {
+                        ModelState.AddModelError(nameof(model.Username), "User is locked.");
+                        return View(model);
+                    }
+
+                    List<Claim> claims = new List<Claim>();
+                    claims.Add(new Claim(ClaimTypes.NameIdentifier, user.UserID.ToString()));
+                    claims.Add(new Claim(ClaimTypes.Name, user.Name ?? string.Empty));
+
+                    claims.Add(new Claim("Username", user.Username));
+
+                    ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                    ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+
+                    HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or password is incorrect.");
+                }
             }
             return View(model);
 
@@ -45,17 +80,62 @@ namespace Hastane.Controllers
         {
             return View();
         }
+
+        private string DoMD5HashedString(string s)
+        {
+            string md5Salt = _configuration.GetValue<string>("AppSettings:MD5Salt");
+            string salted = s + md5Salt;
+            string hashed = salted.MD5();
+            return hashed;
+        }
+
         [HttpPost]
         public IActionResult Register(RegisterViewModel model)
         {
 
+
+
             if (ModelState.IsValid)
             {
 
+                if (_databaseContext.Users.Any(x => x.Username.ToLower() == model.Username.ToLower()))
+                {
+                    ModelState.AddModelError(nameof(model.Username), "Username is already exists.");
+                    View(model);
+                }
+                string hashedPassword = DoMD5HashedString(model.Password);
+                User user = new()
+                {
+                    Username = model.Username,
+                    Name = model.Name,
+                    Surname = model.Surname,
+                    Password = hashedPassword,
+
+                };
+                _databaseContext.Users.Add(user);
+                int affectedRowCount = _databaseContext.SaveChanges();
+
+                if (affectedRowCount == 0)
+                {
+                    ModelState.AddModelError("", "User can not be added.");
+                }
+                else
+                {
+                    return RedirectToAction(nameof(Login));
+                }
             }
             return View(model);
             
         }
+
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction(nameof(Login));
+        }
+
+
+
 
 
     }
